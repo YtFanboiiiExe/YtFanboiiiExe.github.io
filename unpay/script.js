4c50d7eea98999fd2247f3f04d41df38eeb60c93
YtFanboiiiExe <139304491+YtFanboiiiExe@users.noreply.github.com>
2023-10-06 11:53:49 -0700

    Update script.js

--- unpay/script.js
+++ unpay/script.js
@@ -1,11 +1,50 @@
+// JavaScript for navigation - switching between pages and highlighting active link
+const navLinks = document.querySelectorAll('.bottom-nav a');
+const appSections = document.querySelectorAll('.app-list');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        const targetId = link.getAttribute('href').slice(1); // Remove the '#' from the href
+        appSections.forEach(section => {
+            if (section.id === targetId) {
+                section.style.display = 'block';
+            } else {
+                section.style.display = 'none';
+            }
+        });
+
+        // Highlight the active link
+        navLinks.forEach(navLink => navLink.classList.remove('active'));
+        link.classList.add('active');
+    });
+});
+
+// Example app data for all apps
+const apps = [
+    { name: "App 1", icon: "app1.png", description: "Description of App 1" },
+    { name: "App 2", icon: "app2.png", description: "Description of App 2" },
+    // Add more app data here
+];
+
+// Create app cards for all apps
 const appList = document.querySelector(".app-list");
-const featuredList = document.querySelector(".featured-list");
+apps.forEach(app => {
+    const appCard = document.createElement("div");
+    appCard.classList.add("app-card");
+    appCard.innerHTML = `
+        <img src="${app.icon}" alt="${app.name}">
+        <h2>${app.name}</h2>
+        <p>${app.description}</p>
+    `;
+    appList.appendChild(appCard);
+});
 
 // Fetch data from the JSON file for featured apps
 fetch("featured-apps.json")
     .then(response => response.json())
     .then(data => {
         // Create app cards for the featured apps
+        const featuredList = document.querySelector("#featured");
         data.forEach(app => {
             const appCard = document.createElement("div");
             appCard.classList.add("app-card");
@@ -18,22 +57,3 @@ fetch("featured-apps.json")
         });
     })
     .catch(error => console.error("Error loading featured apps:", error));
-
-// Example app data for all apps
-const apps = [
-    { name: "App 1", icon: "app1.png", description: "Description of App 1" },
-    { name: "App 2", icon: "app2.png", description: "Description of App 2" },
-    // Add more app data here
-];
-
-// Create app cards for all apps
-apps.forEach(app => {
-    const appCard = document.createElement("div");
-    appCard.classList.add("app-card");
-    appCard.innerHTML = `
-        <img src="${app.icon}" alt="${app.name}">
-        <h2>${app.name}</h2>
-        <p>${app.description}</p>
-    `;
-    appList.appendChild(appCard);
-});
