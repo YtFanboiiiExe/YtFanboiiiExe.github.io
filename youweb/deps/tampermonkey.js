76aa1e706d3995fdb6ed5e79c69c66ea7d415c5d
YtFanboiiiExe <139304491+YtFanboiiiExe@users.noreply.github.com>
2023-10-18 21:00:01 -0700

    Create tampermonkey.js

---
+++ youweb/deps/tampermonkey.js
@@ -0,0 +1,113 @@
+// ==UserScript==
+// @name         Custom YouTube Tools
+// @namespace    http://your.namespace
+// @version      1.0
+// @description  Enhance YouTube with custom tools
+// @author       YtFanboiii.exe
+// @match        https://www.youtube.com/*   // Specify the URL where the script should run
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let isExpanded = false;
+
+    // Create a container div for the GUI
+    const guiContainer = document.createElement('div');
+    guiContainer.id = 'custom-gui';
+    guiContainer.style.position = 'fixed';
+    guiContainer.style.bottom = '10px';
+    guiContainer.style.right = '10px';
+    guiContainer.style.backgroundColor = 'red';
+    guiContainer.style.border = '1px solid #ccc';
+    guiContainer.style.padding = '10px';
+    guiContainer.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.2)';
+    guiContainer.style.zIndex = '9999';
+
+    // Create a button to toggle the visibility of the GUI content
+    const toggleButton = document.createElement('button');
+    toggleButton.textContent = '▶'; // Arrow pointing right
+    toggleButton.style.padding = '5px';
+    toggleButton.style.border = 'none';
+    toggleButton.style.backgroundColor = 'transparent';
+    toggleButton.style.color = 'white';
+    toggleButton.style.cursor = 'pointer';
+    toggleButton.addEventListener('click', toggleGUI);
+
+    // Create a div for the GUI content (initially hidden)
+    const contentDiv = document.createElement('div');
+    contentDiv.style.display = 'none';
+
+    // Create a title for the GUI
+    const title = document.createElement('h2');
+    title.textContent = 'YouWeb'; // Title
+    title.style.color = 'white'; // Text color
+    title.style.display = 'none';
+
+    // Create a description for the GUI
+    const description = document.createElement('p');
+    description.textContent = 'Made by Cole Cemore (YtFanboiii.exe)'; // Description
+    description.style.color = 'white'; // Text color
+    description.style.display = 'none';
+
+    // Create a button for unblocking the video
+    const unblockButton = document.createElement('button');
+    unblockButton.textContent = 'Unblock the Video';
+    unblockButton.style.backgroundColor = 'white';
+    unblockButton.style.color = 'red';
+    unblockButton.addEventListener('click', unblockVideo);
+
+    // Create a button for downloading the video
+    const downloadButton = document.createElement('button');
+    downloadButton.textContent = 'Download the Video';
+    downloadButton.style.backgroundColor = 'white';
+    downloadButton.style.color = 'red';
+    downloadButton.addEventListener('click', downloadVideo);
+
+    // Add the toggle button to the GUI container
+    guiContainer.appendChild(toggleButton);
+
+    // Add the content div to the GUI container
+    guiContainer.appendChild(contentDiv);
+
+    // Add the title to the content div
+    contentDiv.appendChild(title);
+
+    // Add the description to the content div
+    contentDiv.appendChild(description);
+
+    // Add the unblock button to the content div
+    contentDiv.appendChild(unblockButton);
+
+    // Add the download button to the content div
+    contentDiv.appendChild(downloadButton);
+
+    // Add the GUI container to the document body
+    document.body.appendChild(guiContainer);
+
+    function toggleGUI() {
+        isExpanded = !isExpanded;
+        contentDiv.style.display = isExpanded ? 'block' : 'none';
+        title.style.display = isExpanded ? 'block' : 'none';
+        description.style.display = isExpanded ? 'block' : 'none';
+    }
+
+    function unblockVideo() {
+        if (window.location.toString().includes('www.youtube.com/watch?v')) {
+            window.open('https://www.youtube-nocookie.com/embed/' + window.location.toString().split('=')[1]);
+        }
+    }
+
+    function downloadVideo() {
+        // Load the download script from the provided URL
+        const scriptUrl = 'https://raw.githubusercontent.com/minanagehsalalma/Youtube-Downloader-Bookmarklet/main/WithMenuMimfiyed.js';
+
+        // Create a script element and set its source to the provided URL
+        const script = document.createElement('script');
+        script.src = scriptUrl;
+
+        // Append the script element to the document body
+        document.body.appendChild(script);
+    }
+})();
